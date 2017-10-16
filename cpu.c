11d34f79db0fb6fc786affc39842b47ef3ddd385
Stephen Kang <jkkang@sfu.ca>
2017-10-16 19:08:38 -0400

    macros to add/get registers+flags

--- cpu.c
+++ cpu.c
@@ -1,3 +1,23 @@
+#define SET_AF(x) do {registers.A = ((x & 0xFF00) >> 8); registers.F = (x&0x00FF);} while(0) // multi-line macro
+#define SET_BC(x) do {registers.B = ((x & 0xFF00) >> 8); registers.C = (x&0x00FF);} while(0)
+#define SET_DE(x) do {registers.D = ((x & 0xFF00) >> 8); registers.E = (x&0x00FF);} while(0)
+#define SET_HL(x) do {registers.H = ((x & 0xFF00) >> 8); registers.L = (x&0x00FF);} while(0)
+
+#define GET_AF() ((registers.A << 8) | registers.F)
+#define GET_BC() ((registers.B << 8) | registers.C)
+#define GET_DE() ((registers.D << 8) | registers.E)
+#define GET_HL() ((registers.H << 8) | registers.L)
+
+#define SET_Z(x) ((registers.F & 0x7F) | (x << 7))
+#define SET_N(x) ((registers.F & 0xBF) | (x << 6))
+#define SET_H(x) ((registers.F & 0xDF) | (x << 5))
+#define SET_C(x) ((registers.F & 0xEF) | (x << 4))
+
+#define FLAG_Z ((registers.F >> 7) & 0xF)
+#define FLAG_N ((registers.F >> 6) & 0xF)
+#define FLAG_H ((registers.F >> 5) & 0xF)
+#define FLAG_C ((registers.F >> 4) & 0xF)
+
 struct registers {
     unsigned char A;
     unsigned char F; // flags: Z N H C
